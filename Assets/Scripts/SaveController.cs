4487e4663a3f0d65819ba4bac2f04afbecb56b66
n-dorazio <natedorazio@gmail.com>
2025-05-14 16:37:03 -0600

    Start of UI and Basic Save

---
+++ Assets/Scripts/SaveController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Cinemachine;
+
+public class SaveController : MonoBehaviour
+{
+
+    private string saveLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+
+        LoadGame();
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D.gameObject.name
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        }
+        else
+        {
+            SaveGame();
+        }
+    }
+}
